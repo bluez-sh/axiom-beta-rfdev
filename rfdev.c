ca5235033924070587e526fa893e614ef33bcf12
Swaraj Hota <swarajhota353@gmail.com>
2020-05-20 21:52:02 +0530

    test address consume

--- rfdev.c
+++ rfdev.c
@@ -1,17 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/i2c.h>
 
 #define DEVICE_NAME "rfdev"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Swaraj Hota");
 MODULE_DESCRIPTION("A character driver used to program/debug routing fabrics \
-        in AXIOM Beta main board");
+in AXIOM Beta main board");
 MODULE_VERSION("0.1");
+
+static struct i2c_board_info __initdata rfdev_i2c_board_info[] = {
+    { I2C_BOARD_INFO("pic16", 0x40), },
+    { }
+};
 
 static int __init rfdev_init(void)
 {
     printk(KERN_DEBUG "rfdev: init\n");
+    i2c_register_board_info(2, rfdev_i2c_board_info, 
+                    ARRAY_SIZE(rfdev_i2c_board_info));
     return 0;
 }
 
