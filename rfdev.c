fad9545967772646690b2550c4635a43e16ab84a
Swaraj Hota <swarajhota353@gmail.com>
2020-06-14 18:55:54 +0530

    test: fpga manager skeleton

--- rfdev.c
+++ rfdev.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/i2c.h>
 #include <linux/slab.h>
+#include <linux/fpga/fpga-mgr.h>
 
 #include "rfdev.h"
 
@@ -15,6 +16,7 @@ MODULE_VERSION("0.1");
 #define PIC_NUM_ADDRS 16
 
 static struct device *root_dev;
+static struct fpga_manager *fpga_mgr;
 
 struct rfdev_client {
 	struct i2c_client *client;
@@ -41,6 +43,9 @@ static ssize_t idcode_show(struct device *dev,
 
 	client = dev_get_drvdata(dev);
 	rfdev  = i2c_get_clientdata(client);
+
+	if (!rfdev)
+		return -EFAULT;
 
 	i2c_smbus_write_byte(get_i2c_client(rfdev, PIC_WR_TMS_OUT), 0b11111111);
 	i2c_smbus_write_byte_data(get_i2c_client(rfdev, PIC_WR_TMS_OUT_LEN),
@@ -69,6 +74,41 @@ static struct attribute *dev_attrs[] = {
 
 static struct attribute_group dev_attr_group = {
 	.attrs = dev_attrs,
+};
+
+static int rfdev_fpga_ops_config_init(struct fpga_manager *mgr,
+				      struct fpga_image_info *info,
+				      const char *buf, size_t count)
+{
+	pr_debug("%s: called\n", __func__);
+	return 0;
+}
+
+static int rfdev_fpga_ops_config_write(struct fpga_manager *mgr,
+				       const char *buf, size_t count)
+{
+	pr_debug("%s: called\n", __func__);
+	return 0;
+}
+
+static int rfdev_fpga_ops_config_complete(struct fpga_manager *mgr,
+					  struct fpga_image_info *info)
+{
+	pr_debug("%s: called\n", __func__);
+	return 0;
+}
+
+static enum fpga_mgr_states rfdev_fpga_ops_state(struct fpga_manager *mgr)
+{
+	pr_debug("%s: called\n", __func__);
+	return 0;
+}
+
+static const struct fpga_manager_ops rfdev_fpga_ops = {
+	.write_init	= rfdev_fpga_ops_config_init,
+	.write		= rfdev_fpga_ops_config_write,
+	.write_complete = rfdev_fpga_ops_config_complete,
+	.state		= rfdev_fpga_ops_state,
 };
 
 static int rfdev_make_dummy_client(struct rfdev_device *rfdev,
@@ -150,9 +190,21 @@ static int rfdev_probe(struct i2c_client *client,
 		pr_debug("%s: read/write test failed, received byte 0x%02x\n",
 				__func__, val);
 
+	/* Create and register fpga manager */
+	fpga_mgr = devm_fpga_mgr_create(dev, "RFDev MachXO2 FPGA Manager",
+					&rfdev_fpga_ops, NULL);
+	if (!fpga_mgr)
+		return -ENOMEM;
+
+	err = fpga_mgr_register(fpga_mgr);
+	if (err) {
+		pr_err("%s: unable to register FPGA manager\n", __func__);
+		return err;
+	}
+
 	/* Create sysfs entries */
 	root_dev = root_device_register(DEV_NAME);
-	if (!dev)
+	if (!root_dev)
 		return -ENOMEM;
 	dev_set_drvdata(root_dev, client);
 	err = sysfs_create_group(&root_dev->kobj, &dev_attr_group);
@@ -167,7 +219,11 @@ static int rfdev_remove(struct i2c_client *client)
 	struct rfdev_device *rfdev = i2c_get_clientdata(client);
 
 	pr_debug("%s: remove called\n", DEV_NAME);
-	rfdev_remove_dummy_clients(rfdev);
+
+	if (fpga_mgr)
+		fpga_mgr_unregister(fpga_mgr);
+	if (rfdev)
+		rfdev_remove_dummy_clients(rfdev);
 	if (root_dev)
 		root_device_unregister(root_dev);
 
